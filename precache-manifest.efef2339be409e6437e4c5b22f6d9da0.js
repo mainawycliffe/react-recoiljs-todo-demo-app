253fe2cce0fe01504819951b1540b61ee9767410
Maina Wycliffe <wmmaina7@gmail.com>
2020-07-26 06:14:34 +0300

    Updates

---
+++ precache-manifest.efef2339be409e6437e4c5b22f6d9da0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9374c84641f6a01e2d7172fd8905b3ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07d8c36b2b0f59666efe",
+    "url": "/static/css/2.240f97b7.chunk.css"
+  },
+  {
+    "revision": "5a0c498c7be3670234b6",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "07d8c36b2b0f59666efe",
+    "url": "/static/js/2.0efdfc23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0efdfc23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a0c498c7be3670234b6",
+    "url": "/static/js/main.45084394.chunk.js"
+  },
+  {
+    "revision": "0bd161286feb9cfcc9a1",
+    "url": "/static/js/runtime-main.a8db58b8.js"
+  }
+]);
