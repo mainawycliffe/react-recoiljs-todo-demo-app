2c90c734d2b2563b55e5db2bb95f647edabe553a
Maina Wycliffe <wmmaina7@gmail.com>
2020-07-26 06:17:26 +0300

    Updates

---
+++ precache-manifest.826dee4f89c804939eccbaa1430107ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a9b76fd0b9551eb55b542b003ff4db4",
+    "url": "/react-recoiljs-todo-app/index.html"
+  },
+  {
+    "revision": "07d8c36b2b0f59666efe",
+    "url": "/react-recoiljs-todo-app/static/css/2.240f97b7.chunk.css"
+  },
+  {
+    "revision": "946d78f0a8ea8d401da2",
+    "url": "/react-recoiljs-todo-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "07d8c36b2b0f59666efe",
+    "url": "/react-recoiljs-todo-app/static/js/2.0efdfc23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-recoiljs-todo-app/static/js/2.0efdfc23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "946d78f0a8ea8d401da2",
+    "url": "/react-recoiljs-todo-app/static/js/main.254a884f.chunk.js"
+  },
+  {
+    "revision": "552460fb969b652e37bd",
+    "url": "/react-recoiljs-todo-app/static/js/runtime-main.67b46222.js"
+  }
+]);
